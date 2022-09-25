14adac152ada6481c94717dadb5803c85a5f6436
Extudent76 <110968318+Extudent76@users.noreply.github.com>
2022-09-25 21:33:18 +0300

    Practic_1

---
+++ untitled/src/practic_1_4.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class practic_1_4 {
+    public static void main(String[] args) {
+        System.out.println("Введите количество элементов:");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int n=0, max =0, min=0;
+        int[] array = new int[num];
+        System.out.println("ввод элементов:");
+//заполнение массива
+        for(int i=0;i<num;i++)
+        {
+            array[i] = in.nextInt();
+        }
+//подсчет элементов 1
+        int i=0;
+        while(i<num)
+        {
+
+            n+=array[i];
+                if (max < array[i]) max = array[i];
+                if (i == 0) {
+                    min = array[i];
+                }
+                if (min > array[i]) min = array[i];
+            i++;
+        }
+        System.out.println(n);
+        n=0;
+
+//подсчет элементов 2
+        i=0;
+        do
+        {
+            n+=array[i];
+            i++;
+        }
+        while(i<num);
+        System.out.println(n);
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
---
+++ untitled/src/practic_1_5.java
@@ -0,0 +1,11 @@
+
+public class practic_1_5 {
+    public static void main(String[] args) {
+String element;
+for (int i =0;i< args.length;i++)
+{
+    element = args[i];
+    System.out.println(element);
+}
+    }
+}
